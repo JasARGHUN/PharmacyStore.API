33aa3ad5b2e7d35cb1eedb78cc70b814b3c8d91a
nexus251089 <nexus251089@gmail.com>
2021-06-08 22:11:51 +0600

    Add project files.

---
+++ PharmacyStore.UI/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PharmacyStore.UI.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        public string Image { get; set; }
+
+        [Required]
+        public string Summary { get; set; }
+
+        [Required]
+        public int CompanyId { get; set; }
+        public virtual CompanyViewModel Company { get; set; }
+    }
+}
---
+++ PharmacyStore.UI/Startup.cs
@@ -0,0 +1,75 @@
+using Blazored.LocalStorage;
+using Blazored.Toast;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PharmacyStore.UI.Providers;
+using PharmacyStore.UI.Services;
+using PharmacyStore.UI.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyStore.UI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddBlazoredLocalStorage();
+            services.AddBlazoredToast();
+            services.AddHttpContextAccessor();
+
+            services.AddScoped<APIAuthStateProvider>();
+            services.AddScoped<AuthenticationStateProvider>(x => x.GetRequiredService<APIAuthStateProvider>());
+            services.AddScoped<JwtSecurityTokenHandler>();
+            services.AddHttpClient();
+
+            services.AddTransient<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<ICompanyService, CompanyService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddTransient<IFileUpload, FileUpload>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
